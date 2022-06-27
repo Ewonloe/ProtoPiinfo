c3e19bb286637b554dda91eae6f4aa2ae77d5462
Charlie <carlos.henriquez.r@usach.cl>
2022-06-27 16:42:29 -0400

    Added CORS library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 
 const express = require('express')
+const cors    = require('cors')
 const app 	  = express()
 const port    = 5000
 
 app.use(express.json())
-
+app.use(cors())
 
 app.use('/api', require('./routes/'))
 
